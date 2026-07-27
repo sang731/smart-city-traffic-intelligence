2d189465fd4d9902551452498aeb1be7e56f18fe
Sangamithra <sangamithra1135@gmail.com>
2026-07-27 12:11:44 +0530

    role-based access updates

--- snowflake/rbac.sql
+++ snowflake/rbac.sql
@@ -13,9 +13,9 @@ grant usage on database traffic_intelligence_db to role developer;
 grant usage on database traffic_intelligence_db to role data_analyst;
 
 grant usage on all schemas in database traffic_intelligence_db to role developer;
-grant usage on schema traffic_intelligence_db.gold to role data_analyst;
+grant usage on schema traffic_intelligence_db.dbt_dev_gold to role data_analyst;
 
-grant select, insert, update, delete on all tables in schema traffic_intelligence_db.silver to role developer;
-grant select on all tables in schema traffic_intelligence_db.gold to role data_analyst;
+grant select, insert, update, delete on all tables in schema traffic_intelligence_db.dbt_dev_silver to role developer;
+grant select on all tables in schema traffic_intelligence_db.dbt_dev_gold to role data_analyst;
 
-grant select on future tables in schema traffic_intelligence_db.gold to role data_analyst;
+grant select on future tables in schema traffic_intelligence_db.dbt_dev_gold to role data_analyst;
